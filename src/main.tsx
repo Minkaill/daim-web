75edbd18f7865ff37e204af97e3b8120a9a687dc
Minkaill <mkx.dake@gmail.com>
2025-07-26 23:05:39 +0300

    fix: add telegram provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { Layout } from "./components/Layout/index.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { TelegramProvider } from "./context/telegram/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
+      <TelegramProvider>
+        <Layout>
+          <App />
+        </Layout>
+      </TelegramProvider>
     </BrowserRouter>
   </StrictMode>
 );
