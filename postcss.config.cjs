31fa5f8e90f421c157e84b01d4ca74a6f9423d4b
Minkaill <mkx.dake@gmail.com>
2025-07-26 23:34:14 +0300

    fix: fix postCSS config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,6 @@
-// postcss.config.cjs
 module.exports = {
   plugins: {
-    // вместо 'tailwindcss' теперь '@tailwindcss/postcss'
     "@tailwindcss/postcss": {},
     autoprefixer: {},
-    // если нужно — другие плагины
   },
 };
