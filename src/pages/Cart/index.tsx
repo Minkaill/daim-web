fce0bfc8fb21a48e7088369e648145384ad6006c
Minkaill <mkx.dake@gmail.com>
2025-07-28 14:36:51 +0300

    test: test onClick telegram Back Button

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { useTelegram } from "../../context/telegram";
+import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
+  const navigate = useNavigate();
+
   const { tg } = useTelegram();
 
   useEffect(() => {
     if (tg) {
       tg.BackButton.show();
+      tg.BackButton.onClick(() => navigate(-1));
     }
   }, []);
 
