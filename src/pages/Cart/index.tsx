f5e737e3b6898ac3f7c24ce5c345dfdb5f694d13
Minkaill <mkx.dake@gmail.com>
2025-07-28 14:41:20 +0300

    feat: add cart page, add header telegram control

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -4,15 +4,20 @@ import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
   const navigate = useNavigate();
-
   const { tg } = useTelegram();
 
   useEffect(() => {
-    if (tg) {
-      tg.BackButton.show();
-      tg.BackButton.onClick(() => navigate(-1));
-    }
-  }, []);
+    if (!tg) return;
+    const onBack = () => navigate(-1);
+
+    tg.BackButton.show();
+    tg.BackButton.onClick(onBack);
+
+    return () => {
+      tg.BackButton.offClick(onBack);
+      tg.BackButton.hide();
+    };
+  }, [tg, navigate]);
 
   return <div>Cart</div>;
 };
